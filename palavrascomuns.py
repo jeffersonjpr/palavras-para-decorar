73f378ba9d18c81ef6eb5d9dded39e5085cd7941
jeffersonjpr <jefferson_michael407@hotmail.com>
2018-01-18 16:38:00 -0200

    Versao 1.2: Mudanças: Opção de listar palavras guardadas.

--- palavrascomuns.py
+++ palavrascomuns.py
@@ -4,30 +4,58 @@ import os
 arq = open('dicionario.pck','rb')
 dicio = pickle.load(arq)
 arq.close()
-while True:
-    print()
-    print("Digite sucodeuva para parar")
-    b= str(input("Digite a palavra: ")).lower()
-    b = b.replace(' ','')
-    os.system('CLS')
-    print("Palavra:",b)
-    while b == '':
-        print('Palavra invalida!!')
-        b= str(input("Digite a palavra: ")).lower()
-        b = b.replace(' ','')
-    if b == 'sucodeuva':
-        break
-    if b not in dicio:
-        print('A palavra %s nunca foi vista!Adicionada ao programa.'%b)
-        dicio[b] = 1
-    else:
-        if dicio[b] >= 3:
-            print("A palavra %s ja foi vista %i vezes,adicione no anki!!"%(b,dicio[b]))
-            dicio[b]+=1
-        else:
-            print("A palavra ja foi vista porem ainda nao atingiu os pontos necessarios.")
-            dicio[b]+=1
+def litado():
+    lista = []
+    for key,value in sorted(dict.items(dicio)):
+        lista.append('%-15s %s'%(key,value))
+    while lista != []:
+        print(' | ',end = '')
+        for i in range(3):
+            try:
+                print(lista[i], end=' | ')
+            except:
+                pass
+        print('\n')
+        for i in range(3):
+            try:
+                del lista[0]
+            except:
+                pass
+def save():
     arq = open('dicionario.pck','wb')
     pickle.dump(dicio,arq)
     arq.close()
+while True:
+    print()
+    print("Digite sucodeuva para parar")
+    print("Digite listado para listar")
+    b= str(input("Digite a palavra: ")).lower()
+    b = b.replace(' ','')
+    os.system('CLS')
+    while b == '':
+        os.system('CLS')
+        print('Palavra invalida!!')
+        print("Digite sucodeuva para parar")
+        print("Digite listado para listar")
+        b= str(input("Digite a palavra: ")).lower()
+        b = b.replace(' ','')
+        os.system('CLS')
+    if b == 'sucodeuva':
+        break
+    if b == 'listado':
+        litado()
+        continue
+    if b not in dicio:
+        print('A palavra \"%s\" nunca foi vista!Adicionada ao programa.'%b)
+        dicio[b] = 1
+    else:
+        if dicio[b] >= 3:
+            print("A palavra \"%s\" ja foi vista %i vezes,adicione no anki!!"%(b,dicio[b]))
+            dicio[b]+=1
+        else:
+            print("A palavra \"%s\" ja foi vista porem ainda nao atingiu os pontos necessarios." %b)
+            dicio[b]+=1
+    save()
+
+
 
